705e43d9f89be964a530630ddf117ae58cdd3785
Abhijith Kumar A <abhijith.a@experionglobal.local>
2021-05-05 14:11:13 +0530

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,41 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { EmployeesComponent } from './employees/employees.component';
+import { ManageEmployeeComponent } from './employees/manage-employee/manage-employee.component';
+import { DepartmentsComponent } from './departments/departments.component';
+import { ManageDepartmentComponent } from './departments/manage-department/manage-department.component';
+import { AssetRequestsComponent } from './asset-requests/asset-requests.component';
+import { AssetListComponent } from './asset-list/asset-list.component';
+import { ManageAssetComponent } from './asset-list/manage-asset/manage-asset.component';
+import { RouterModule } from '@angular/router';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    EmployeesComponent,
+    ManageEmployeeComponent,
+    DepartmentsComponent,
+    ManageDepartmentComponent,
+    AssetRequestsComponent,
+    AssetListComponent,
+    ManageAssetComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: '', redirectTo: '/homepage', pathMatch: 'full' },
+      { path: 'employees', component: EmployeesComponent },
+      { path: 'departments', component: DepartmentsComponent },
+      { path: 'asset-list', component: AssetListComponent },
+      { path: 'asset-requests', component: AssetRequestsComponent },
+      { path: 'profile', component: ProfileComponent }
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/asset-requests/asset-requests.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-asset-requests',
+  templateUrl: './asset-requests.component.html',
+  styleUrls: ['./asset-requests.component.css']
+})
+export class AssetRequestsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
