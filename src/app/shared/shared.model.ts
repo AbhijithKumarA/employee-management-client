4085fa7cb5d6036e2890f7744cff3caa46996fe1
Varshini A <varshini.a@experionglobal.com>
2021-05-11 11:42:04 +0530

    asset

--- src/app/shared/shared.model.ts
+++ src/app/shared/shared.model.ts
@@ -2,11 +2,18 @@ export class Employee {
     employeeId: number = 0;
     firstName: string = '';
     lastName: string = '';
-    dateOfBirth: string = '';
+    dateOfBirth: Date;
     gender: string = '';
     address: string = '';
-    department: string = '';
-    dateOfJoining: string = '';
+    departmentName: string = '';
+    dateOfJoining: Date;
+}
+
+export class Department {
+    departmentId: number = 0;
+    departmentName: any;
+    deptInChargeName: any;
+    employeeId : number = 0;
 }
 
 export class Asset {
